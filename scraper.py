1c7ecf24d05522ddc0b84c31876e1a3b303eb403
Maroun Baydoun <maroun.baydoun@gmail.com>
2020-12-05 00:36:59 +0200

    Add logging

--- scraper.py
+++ scraper.py
@@ -7,6 +7,8 @@ from datetime import datetime,timezone
 response = requests.get('https://www.instagram.com/marounbaydoun?hl=en')
 html = response.text
 
+print('Fetched HTML')
+
 follower_matches = re.search('\"edge_followed_by\":\{"count":(\d+)\}', html)
 following_matches = re.search('\"edge_follow\":\{"count":(\d+)\}', html)
 
@@ -20,6 +22,7 @@ if follower_count is None or following_count is None:
 now_utc = datetime.now(timezone.utc)
 fields=[now_utc.isoformat(), follower_count, following_count]
 
+print('Will store the following data:', fields)
 
 with open(f'data/{now_utc.month}-{now_utc.year}.csv', 'a') as f:
     writer = csv.writer(f)
